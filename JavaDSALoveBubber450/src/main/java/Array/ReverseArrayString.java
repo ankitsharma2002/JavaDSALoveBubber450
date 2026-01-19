3baeaa0291efee7b9381687b77a43193a9df4623
Ankit Sharma <ankitsharma8102002@gmail.com>
2026-01-19 23:14:23 +0530

    Question 1 reverse array/string complete.

--- JavaDSALoveBubber450/src/main/java/Array/ReverseArrayString.java
+++ JavaDSALoveBubber450/src/main/java/Array/ReverseArrayString.java
@@ -1,7 +1,53 @@
 package Array;
 
 public class ReverseArrayString {
-	public static void main(String[] args) {
-
-	}
+	
+    // Approach 1: Two Pointer (Most Optimized - O(n) time, O(1) space)
+    public static String reverseStringOptimized(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left < right) {
+            // Swap characters
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        
+        return new String(arr);
+    }
+    
+    // Approach 2: Using StringBuilder (O(n) time, O(n) space)
+    public static String reverseStringBuilder(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+    
+    // Approach 3: Recursive (O(n) time, O(n) space due to call stack)
+    public static String reverseStringRecursive(String s) {
+        if (s.isEmpty()) {
+            return s;
+        }
+        return reverseStringRecursive(s.substring(1)) + s.charAt(0);
+    }
+    
+    public static void main(String[] args) {
+        // Test cases
+        String test1 = "Geeks";
+        String test2 = "for";
+        String test3 = "a";
+        
+        System.out.println("Using Two Pointer Approach (Most Optimized):");
+        System.out.println("Input: " + test1 + " -> Output: " + reverseStringOptimized(test1));
+        System.out.println("Input: " + test2 + " -> Output: " + reverseStringOptimized(test2));
+        System.out.println("Input: " + test3 + " -> Output: " + reverseStringOptimized(test3));
+        
+        System.out.println("\nUsing StringBuilder:");
+        System.out.println("Input: " + test1 + " -> Output: " + reverseStringBuilder(test1));
+        
+        System.out.println("\nUsing Recursion:");
+        System.out.println("Input: " + test1 + " -> Output: " + reverseStringRecursive(test1));
+    }
 }
